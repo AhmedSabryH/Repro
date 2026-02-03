0feb58a76ca4686ed12d36aa1228ceb5485d8727
AhmedSabryH <as0162121@gmail.com>
2026-02-03 11:43:15 +0200

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ __('app.repro_tool') }}</title>
+    <title>Repro Tool</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
